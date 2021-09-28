32749d6842a370dc227b57fd2ba3cb51e1254b8a
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-09-28 12:04:39 -0300

    criado os componentes e o reduce

---
+++ src/store/modules/fruits/reducer.js
@@ -0,0 +1,10 @@
+const initialState = ["Banana", "Maça", "Morango", "Uva"];
+
+const fruitsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default fruitsReducer;
